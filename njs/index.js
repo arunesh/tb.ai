caa0fdd4f2f0d962278e0b1e155f8c4830d31e9d
Arunesh Mishra <aruneshm@gmail.com>
2022-12-04 09:42:50 -0700

    first push to server

--- njs/index.js
+++ njs/index.js
@@ -5,5 +5,15 @@ var express = require('express');
 var app = express();
 
 app.get('/', (req, resp) => {
-    return resp.send('OK');
+    return resp.send('TALKBOX DHS OK');
 });
+
+// Start the server
+const PORT = parseInt(process.env.PORT) || 8080;
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+  console.log('Press Ctrl+C to quit.');
+});
+// [END gae_node_request_example]
+
+module.exports = app;
