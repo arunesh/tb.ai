d3411c2da26a73e282deebfed195bc65a5cb776b
Arunesh Mishra <aruneshm@gmail.com>
2022-12-04 20:30:03 -0700

    added bootstrap

--- njs/index.js
+++ njs/index.js
@@ -1,3 +1,9 @@
+//////////////////////////////////////////////////
+// NodeJs ExpressJs server for Talkbox DHS app.
+// 
+
+// Code comes from the samples: https://github.com/GoogleCloudPlatform/nodejs-docs-samples/tree/main/appengine
+
 "use strict";
 
 var express = require('express');
@@ -8,12 +14,23 @@ app.get('/', (req, resp) => {
     return resp.send('TALKBOX DHS OK');
 });
 
+app.post('/', (req, resp) => {
+    return resp.send('Got a POST Requst.');
+});
 // Start the server
 const PORT = parseInt(process.env.PORT) || 8080;
+
+
+console.log("Running port = " + PORT);
+
+
+// app.listen is the blocking call. 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
   console.log('Press Ctrl+C to quit.');
 });
-// [END gae_node_request_example]
+
+
+// Good one on module.exports vs exports: https://www.sitepoint.com/understanding-module-exports-exports-node-js/
 
 module.exports = app;
