ec50c15444e3608e0690dbf7899838b3b8ab7133
Arunesh Mishra <aruneshm@gmail.com>
2022-12-27 20:40:01 -0700

    sql commands

--- njs/lib/dblayer.js
+++ njs/lib/dblayer.js
@@ -12,10 +12,15 @@ const db = new sqlite3.Database(':memory:', sqlite3.OPEN_CREATE);
 // Username, Name, credentials (pwd), last login, teamid, admin flag
 //
 // Translations table:
-// TranslationID, username, source language, target language, audio filepath, original text, translated text, ticketid
+// TranslationID, username, source language, target language, audio filepath, source language text, target language translated text, ticketid
 //
-// Tickets table:
+// Tickets table OLD:
 // TicketID, TranslationID (list), list of comments
+//
+// Tickets table NEW:
+// CorrectSrcLangText can be NULL
+// CorrectTargetLangText can be NULL but one must be non NULL
+// TicketID, TranslationID, CreationDate, LastUpdated, CorrectSrcLangText, CorrectTargetLangText, JSON-list of comments
 //
 db.serialize(() => {
     db.run("CREATE TABLE lorem (info TEXT)");
@@ -30,6 +35,49 @@ db.serialize(() => {
         console.log(row.id + ": " + row.info);
     });
 });
+
+function createTables() {
+    // User table:
+    db.serialize(() => {
+        const user_table_sql =
+            "CREATE TABLE Users (UserName varchar(255)," +
+            "Name varchar(255), " +
+            "Cred varchar(255)," +
+            "LastLogin DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP," +
+            "TeamId SMALLINT(255)," +
+            "Admin BIT(1))";
+
+        db.run(user_table_sql);
+    }
+
+    // Translations table:
+    db.serialize(() => {
+        const trans_table_sql = 
+            "CREATE TABLE Translations (TranslationId INT(255)," +
+            "UserName varchar(255)," +
+            "SrcLang varchar(255)," +
+            "TargetLang varchar(255)," +
+            "AudioFilePath varchar(3000)," +
+            "SrcLangText varchar(3000)," +
+            "TargetLangText varchar(3000)," +
+            "TicketId INT(255))");
+
+        db.run(trans_table_sql);
+    }
+
+    // Tickets table:
+    db.serialize(() => {
+        const tickets_table_sql =
+            "CREATE TABLE Tickets (TicketId INT(255), " +
+            "TranslationId INT(255), " +
+            "CreationDate DATETIME DEFAULT CURRENT_TIMESTAMP, " + 
+            "LastUpdated DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, " +
+            "CorrectSrcLangText varchar(3000), " +
+            "CorrectTargetLangText varchar(3000), " +
+            "Comments varchar(10000))");
+        db.run(tickets_table_sql);
+    }
+}
 
 function helloDb() {
     console.log("Hello from the DB Layer");
