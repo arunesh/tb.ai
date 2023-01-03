6cde41cb6ebc4b70c6654af6313a32d365415b81
Arunesh Mishra <aruneshm@gmail.com>
2023-01-02 17:35:44 -0700

    minor update

--- njs/lib/dbtest.js
+++ njs/lib/dbtest.js
@@ -1,4 +1,9 @@
-
+/**
+ * Use this to create, populate or otherwise inspect an sqlite3 database.
+ *
+ * node dblayer.js should bring up a repl with some exported methods.
+ *
+ */
 const sqlite3 = require('sqlite3').verbose();
 const repl = require('node:repl');
 const fs = require('fs');
@@ -21,13 +26,45 @@ function openDb(dbName) {
 }
 
 let db = openDb(DB_NAME);
+db.on('error', err => {
+    console.log(err);
+    process.exit(-1);
+});
+
+db.on('trace', sql => {
+    console.log(sql);
+});
+
+
+/**
+ * Returns a Promise.
+ */
+function dropTableIfExists(tablename) {
+    return new Promise((resolve, reject) => {
+        db.run(`DROP TABLE IF EXISTS ${tablename}`,
+            [ ],
+            err => {
+                if (err) reject(err);
+                else resolve();
+            })
+    });
+}
 
 function loadFromFile(filename, tablename) {
+
+    var stmt = db.prepare("INSERT INTO users VALUES (?,?)");
+
     fs.createReadStream(filename)
         .pipe(parse({ delimiter: ",", from_line: 2 }))
         .on("data", function (row) {
             console.log(row);
         })
+        .on("end", function () {
+            console.log("finished");
+        })
+        .on("error", function (error) {
+            console.log(error.message);
+        });
 }
 
 function dumpDb() {
